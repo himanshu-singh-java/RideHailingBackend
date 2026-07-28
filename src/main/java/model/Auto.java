b2fab440fc81ded9ed5915522fa0f06d78995d62
Himanshu Kumar <singhhimanshu85942@gmail.com>
2026-07-28 23:06:33 +0530

    feat: added Bike and Auto models with real-world attributes and fare logic

---
+++ src/main/java/model/Auto.java
@@ -0,0 +1,77 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+
+@Entity
+@DiscriminatorValue("AUTO")
+public class Auto extends Vehicles{
+
+    @Column(name = "auto_model")
+    private String autoModel;
+
+    @Column(name = "permit_type")
+    private String permitType;
+
+    @Column(name = "passenger_capacity")
+    private int passengerCapacity;
+
+    @Column(name = "is_shared_auto")
+    private boolean isSharedAuto;
+
+    public Auto(){
+
+    }
+
+    @Override
+    public double calculateFare(double distanceKm){
+        double baseFare = 50;
+        double ratePerKm = 15;
+
+        return baseFare + (distanceKm * ratePerKm);
+    }
+
+    public String getAutoModel() {
+        return autoModel;
+    }
+
+    public void setAutoModel(String autoModel) {
+        this.autoModel = autoModel;
+    }
+
+    public String getPermitType() {
+        return permitType;
+    }
+
+    public void setPermitType(String permitType) {
+        this.permitType = permitType;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public boolean isSharedAuto() {
+        return isSharedAuto;
+    }
+
+    public void setSharedAuto(boolean sharedAuto) {
+        isSharedAuto = sharedAuto;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "autoModel='" + autoModel + '\'' +
+                ", permitType='" + permitType + '\'' +
+                ", passengerCapacity=" + passengerCapacity +
+                ", isSharedAuto=" + isSharedAuto +
+                '}';
+    }
+}
---
+++ src/main/java/model/Bike.java
@@ -0,0 +1,88 @@
+package model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("BIKE")
+public class Bike extends Vehicles {
+
+    @Column(name = "bike_model")
+    private String bikeModel;
+
+    @Column(name = "bike_type")
+    private String bikeType;
+
+    @Column(name = "passenger_capacity")
+    private int passengerCapacity;
+
+    @Column(name = "has_extra_helmet")
+    private boolean hasExtraHelmet;
+
+    @Column(name = "is_parcel_delivery_enabled")
+    private boolean isParcelDeliveryEnabled;
+
+    public Bike(){
+
+    }
+
+    @Override
+    public double calculateFare(double distanceKm){
+        double baseFare = 20;
+        double perKmRate = 6;
+        return baseFare + (perKmRate * distanceKm);
+    }
+
+    public String getBikeModel() {
+        return bikeModel;
+    }
+
+    public void setBikeModel(String bikeModel) {
+        this.bikeModel = bikeModel;
+    }
+
+    public String getBikeType() {
+        return bikeType;
+    }
+
+    public void setBikeType(String bikeType) {
+        this.bikeType = bikeType;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public boolean isHasExtraHelmet() {
+        return hasExtraHelmet;
+    }
+
+    public void setHasExtraHelmet(boolean hasExtraHelmet) {
+        this.hasExtraHelmet = hasExtraHelmet;
+    }
+
+    public boolean isParcelDeliveryEnabled() {
+        return isParcelDeliveryEnabled;
+    }
+
+    public void setParcelDeliveryEnabled(boolean parcelDeliveryEnabled) {
+        isParcelDeliveryEnabled = parcelDeliveryEnabled;
+    }
+
+    @Override
+    public String toString() {
+        return "Bike{" +
+                "bikeModel='" + bikeModel + '\'' +
+                ", bikeType='" + bikeType + '\'' +
+                ", passengerCapacity=" + passengerCapacity +
+                ", hasExtraHelmet=" + hasExtraHelmet +
+                ", isParcelDeliveryEnabled=" + isParcelDeliveryEnabled +
+                '}';
+    }
+}
